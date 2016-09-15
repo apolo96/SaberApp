2cdb3084a56ce8f3c3a0aa5b2a45eb1468b618ee
apolo96 <andresfpvclap@gmail.com>
2016-09-14 23:32:08 -0500

    Initial commit

--- SaberApp/Question4.Designer.cs
+++ SaberApp/Question4.Designer.cs
@@ -28,21 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnNext = new MaterialSkin.Controls.MaterialFlatButton();
             this.SuspendLayout();
+            // 
+            // btnNext
+            // 
+            this.btnNext.AutoSize = true;
+            this.btnNext.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnNext.Depth = 0;
+            this.btnNext.Location = new System.Drawing.Point(294, 235);
+            this.btnNext.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnNext.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Primary = false;
+            this.btnNext.Size = new System.Drawing.Size(46, 36);
+            this.btnNext.TabIndex = 13;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
             // 
             // Question4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(383, 308);
+            this.Controls.Add(this.btnNext);
             this.Name = "Question4";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Question Numbre Four";
             this.Load += new System.EventHandler(this.Question4_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private MaterialSkin.Controls.MaterialFlatButton btnNext;
     }
 }
